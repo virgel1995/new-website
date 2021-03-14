b8d9d8e2528eef2fa22c0c8a9997554960ddf091
virgel1995 <72863390+virgel1995@users.noreply.github.com>
2021-03-14 11:48:51 +0200

    some updates
    changed css back-to-top class to another name
    added whatsapp icon when scroling

--- js/script.js
+++ js/script.js
@@ -34,10 +34,21 @@ $(document).ready(function () {
 $(window).scroll(function () {
     let position = $(this).scrollTop();
     if (position >= 718) {
-        $("#back-to-top").addClass("scrollTop");
+        $("#back-to-top").addClass("scrollTop-back-to-top");
 
     } else {
-        $("#back-to-top").removeClass("scrollTop");
+        $("#back-to-top").removeClass("scrollTop-back-to-top");
+
+    }
+})
+//  whatsapp 
+$(window).scroll(function () {
+    let position = $(this).scrollTop();
+    if (position >= 718) {
+        $("#whatsap-link").addClass("scrollTop-whatsap");
+
+    } else {
+        $("#whatsap-link").removeClass("scrollTop-whatsap");
 
     }
 })
@@ -48,10 +59,10 @@ $(window).scroll(function () {
      * ripples jQuary header animation
      * https://github.com/sirxemic/jquery.ripples
      */
-    $("#header, .info").ripples({
-        dropRadius: 10,
-        perturbance: 0.6,
-      });
+    // $("#header, .info").ripples({
+    //     dropRadius: 10,
+    //     perturbance: 0.6,
+    //   });
 
 
     /**
